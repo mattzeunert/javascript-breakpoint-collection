6c5c1c789f37b33aeff0872751a2f5418492bbf0
Matt Zeunert <matt@mostlystatic.com>
2016-04-05 20:21:05 +0100

    Init.

---
+++ app/app.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+export default class App extends React.Component {
+    render(){
+        return <div> rendered by react</div>
+    }
+}
---
+++ show-panel.js
@@ -0,0 +1,8 @@
+chrome.devtools.panels.create("Breakpoint Collection",
+    null,
+    "panel.html",
+    function(panel) {
+        
+      // code invoked on panel creation
+    }
+);
