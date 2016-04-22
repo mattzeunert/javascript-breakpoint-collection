d76c68e66a7bc7b23b0f8a8439f773c1668969e0
Matt Zeunert <matt@mostlystatic.com>
2016-04-22 03:13:48 +0100

    Test debugPropertyGet trace support.

--- tests/test-spec.js
+++ tests/test-spec.js
@@ -3,10 +3,18 @@ import "../extension/javascript-breakpoint-collection"
 describe("debugPropertyGet", function(){
     it("calls a callback before a property is read", function(){
         var obj = {hi: "there"};
-        var called = false;
         var fn = jasmine.createSpy();
         breakpoints.debugPropertyGet(obj, "hi", fn)
         var hi = obj.hi;
         expect(fn).toHaveBeenCalled();
     })
+
+    it("can show a trace message when a property is read", function(){
+        var obj = {hi: "there"};
+        var trace = spyOn(console, "trace")
+        breakpoints.debugPropertyGet(obj, "hi", "trace");
+        var hi = obj.hi;
+        expect(trace).toHaveBeenCalled();
+        expect(trace.calls.mostRecent().args[0]).toEqual("About to get property 'hi' on this object: ");
+    });
 })
