021e201e1feb943db8d2c301a5be69e0e6f4baa0
Matt Zeunert <matt@mostlystatic.com>
2016-04-22 03:18:14 +0100

    Tests for debugPropertyGet, debugPropertyCall.

--- tests/test-spec.js
+++ tests/test-spec.js
@@ -1,7 +1,7 @@
 import "../extension/javascript-breakpoint-collection"
 
 describe("debugPropertyGet", function(){
-    it("calls a callback before a property is read", function(){
+    it("Calls a callback before a property is read", function(){
         var obj = {hi: "there"};
         var fn = jasmine.createSpy();
         breakpoints.debugPropertyGet(obj, "hi", fn)
@@ -9,7 +9,7 @@ describe("debugPropertyGet", function(){
         expect(fn).toHaveBeenCalled();
     })
 
-    it("can show a trace message when a property is read", function(){
+    it("Can show a trace message when a property is read", function(){
         var obj = {hi: "there"};
         var trace = spyOn(console, "trace")
         breakpoints.debugPropertyGet(obj, "hi", "trace");
@@ -17,4 +17,28 @@ describe("debugPropertyGet", function(){
         expect(trace).toHaveBeenCalled();
         expect(trace.calls.mostRecent().args[0]).toEqual("About to get property 'hi' on this object: ");
     });
+})
+
+describe("debugPropertySet", function(){
+    it("Calls a callback when an object property is set", function(){
+        var obj = {hi: "there"};
+        var fn = jasmine.createSpy();
+        breakpoints.debugPropertySet(obj, "hi", fn);
+        obj.hi = "hello"
+        expect(fn).toHaveBeenCalled();
+    })
+});
+
+describe("debugPropertyCall", function(){
+    it("Calls a callback when an object property containing a function is called", function(){
+        var obj = {
+            sayHello: function(){
+                var hi = "hello";
+            }
+        }
+        var fn = jasmine.createSpy();
+        breakpoints.debugPropertyCall(obj, "sayHello", fn);
+        obj.sayHello();
+        expect(fn).toHaveBeenCalled();
+    })
 })
