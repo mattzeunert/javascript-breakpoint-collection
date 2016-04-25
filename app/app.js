c992c606bf2c87bb93981428d7f0cc35558226d6
Matt Zeunert <matt@mostlystatic.com>
2016-04-25 08:49:01 +0100

    Log errors that occur in eval'd script.

--- app/app.js
+++ app/app.js
@@ -7,16 +7,12 @@ var appState = {
 
 export function activateBreakpoint(breakpoint, options){
     var code = "window.breakpoints.__internal.createSpecificBreakpoint('" + breakpoint.title + "')"
-    chrome.devtools.inspectedWindow.eval(code, function(){
-        
-    });
+    evalInInspectedWindow(code);
 }
 
 export function deactivateBreakpoint(breakpoint) {
     var code = "breakpoints.__internal.disableBreakpoint(" + breakpoint.id + ");"
-    chrome.devtools.inspectedWindow.eval(code, function(){
-        
-    })   
+    evalInInspectedWindow(code);
 }
 
 export function updateBreakpoint(breakpoint, traceOrDebugger){
@@ -24,22 +20,29 @@ export function updateBreakpoint(breakpoint, traceOrDebugger){
     var details = Object.assign({}, breakpoint.details);
     details.type = traceOrDebugger;
     var code = "breakpoints.__internal.updateBreakpoint('"+ id + "', " + JSON.stringify(details) + ");"
-    chrome.devtools.inspectedWindow.eval(code, function(regBp){
+    evalInInspectedWindow(code, function(){
         
     })
 }
 
 export function setTypeOfMostRecentBreakpointToDebugger(){
-    evalInInspectedWindow("breakpoints.__internal.setTypeOfMostRecentBreakpointToDebugger()", function(){
-        
-    })
+    evalInInspectedWindow("breakpoints.__internal.setTypeOfMostRecentBreakpointToDebugger()")
 }
 
 
 readBreakpointsFromPage();
 
 function evalInInspectedWindow(code, callback){
-    chrome.devtools.inspectedWindow.eval(code, callback);
+    chrome.devtools.inspectedWindow.eval(code, function(result, err){
+        if (err && err.isException) {
+            console.log("Exception occured in eval'd code", err.value)
+        }
+        else {
+            if (callback) {
+                callback(result);
+            }
+        }
+    });
 }
 
 function readBreakpointsFromPage(){
