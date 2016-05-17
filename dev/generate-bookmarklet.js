e9995db916b232af68970e971e3542dd7c446b5c
Matt Zeunert <matt@mostlystatic.com>
2016-05-17 23:15:23 +0100

    Show link to API docs when loading bookmarklet

--- dev/generate-bookmarklet.js
+++ dev/generate-bookmarklet.js
@@ -3,4 +3,4 @@ var UglifyJS = require("uglify-js");
 var result = UglifyJS.minify("./extension/build/javascript-breakpoint-collection.js", {
     compress: false
 });
-console.log("javascript:eval(decodeURIComponent('" + encodeURIComponent(result.code).replace(/'/g, "\\'") + "'))");
+console.log("javascript:window.__BP_SHOW_CONSOLE_API_MESSAGE = true;eval(decodeURIComponent('" + encodeURIComponent(result.code).replace(/'/g, "\\'") + "'))");
