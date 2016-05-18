1f77bb965827a940a134d34c8e21cb92e5a933d8
Matt Zeunert <matt@mostlystatic.com>
2016-05-18 01:53:39 +0100

    Explain why target can't be UMD

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = [{
   output: {
       path: __dirname + "/extension/build",
       filename: '[name].js',
-      libraryTarget: "var"
+      libraryTarget: "var" // Should NOT be umd, because then the extension breaks on pages with a global `define` function
   },
   module: {
     loaders: [
